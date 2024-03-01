b6cdd68f7c42014ea51cebc25e905d1e7eca9f15
Ceiberon <ethem_641@hotmail.com>
2024-03-01 20:12:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+
+    static int uslu(int a, int b) {
+        if (b == 0)
+            return 1;
+        int result = 1;
+        return a * uslu(a, b - 1);
+        }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Tabanı giriniz: ");
+        int a = scan.nextInt();
+
+        System.out.print("Üssü giriniz: ");
+        int b = scan.nextInt();
+
+
+        int uslu = uslu(a, b);
+        System.out.println("Sonuç: " + uslu);
+    }
+}
+
